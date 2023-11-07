e391b7862a6d7e3544805ea72aad992e8b41d7f1
Kerigo Ndirangu <62352071+kgndirangu@users.noreply.github.com>
2023-11-06 22:05:25 -0500

    analysis questions 5-7

--- EmployeeSQL/SQL_analysis.sql
+++ EmployeeSQL/SQL_analysis.sql
@@ -21,12 +21,42 @@ ON dm.dept_no = d.dept_no
 
 
 -- List the department number for each employee along with that employee’s employee number, last name, first name, and department name.
+-- will need employees table (last name, first name), departments(dept_name, dept_no), need dept_emp table to link these 2 tables   
+-- some employees work in 2 departments and are thus listed twice (e.g. 10010)
+SELECT d.dept_no, e.emp_no, e.last_name, e.first_name, d.dept_name  
+FROM employees as e 
+INNER JOIN dept_emp as de
+ON e.emp_no = de.emp_no
+INNER JOIN departments as d
+ON de.dept_no = d.dept_no
 
 -- List first name, last name, and sex of each employee whose first name is Hercules and whose last name begins with the letter B.
+--will only need employees table, there are 20 employees meeting the criterion
+--https://www.postgresql.org/docs/current/functions-matching.html#FUNCTIONS-POSIX-REGEXP
+SELECT e.first_name, e.last_name, e.sex
+FROM employees as e 
+WHERE e.first_name = 'Hercules' and e.last_name LIKE 'B%' 
 
 -- List each employee in the Sales department, including their employee number, last name, and first name.
+--52,245 rows 
+SELECT d.dept_name, e.emp_no, e.last_name, e.first_name 
+FROM employees as e 
+INNER JOIN dept_emp as de
+ON e.emp_no = de.emp_no
+INNER JOIN departments as d
+ON de.dept_no = d.dept_no
+WHERE d.dept_name = 'Sales'
+
 
 -- List each employee in the Sales and Development departments, including their employee number, last name, first name, and department name.
+--137952 rows
+SELECT  e.emp_no, e.last_name, e.first_name, d.dept_name 
+FROM employees as e 
+INNER JOIN dept_emp as de
+ON e.emp_no = de.emp_no
+INNER JOIN departments as d
+ON de.dept_no = d.dept_no
+WHERE d.dept_name = 'Sales' or d.dept_name = 'Development'
 
 -- List the frequency counts, in descending order, of all the employee last names (that is, how many employees share each last name).
 
